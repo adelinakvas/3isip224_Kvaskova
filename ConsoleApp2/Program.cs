e6786585991c4988a068adbfaaedabdd5e415c00
adelinakvas <adelinakvaskova922@gmail.com>
2026-09-09 10:43:33 +0300

    ввод количества операций с валидацией

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            while (true)
+            {
+                Console.Write("Введите количество операций (от 2 до 40): ");
+                if (int.TryParse(Console.ReadLine(), out count) && count >= 2 && count <= 40)
+                {
+                    break;
+                }
+                Console.WriteLine("Ошибка! Нужно ввести число от 2 до 40.");
+            }
+
         }
     }
 }
