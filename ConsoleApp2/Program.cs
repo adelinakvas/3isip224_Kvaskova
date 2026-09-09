6893709eaf4a41990aa04de2e4cfa1c1347fcbc7
adelinakvas <adelinakvaskova922@gmail.com>
2026-09-09 10:46:14 +0300

    создаем два массива

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -20,6 +20,8 @@ namespace ConsoleApp2
                 }
                 Console.WriteLine("Ошибка! Нужно ввести число от 2 до 40.");
             }
+            string[] names = new string[count];
+            double[] prices = new double[count];
 
         }
     }
